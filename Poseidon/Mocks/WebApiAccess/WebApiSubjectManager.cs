1fe51b0df39bdf295a1b587863cc0b8ee1958d0a
Peter Kiraly <zsonglorman@gmail.com>
2017-10-08 23:12:38 +0200

    Add web api client implementations

--- Poseidon/Mocks/WebApiAccess/WebApiSubjectManager.cs
+++ Poseidon/Mocks/WebApiAccess/WebApiSubjectManager.cs
@@ -1,35 +1,85 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
 using Interfaces;
+using Newtonsoft.Json;
 
 namespace Mocks.Factory
 {
     class WebApiSubjectManager : ISubjectManager
     {
+        private static readonly HttpClient Client = new HttpClient();
+
+        private const string ApiBaseAddress = "http://localhost:58908/";
+
         public List<Subject> GetSubjects()
         {
-            // TODO call web API and return results of the call
-            return new List<Subject>()
+            if (Client.BaseAddress == null)
             {
-                new Subject(1, "A programozás alapjai 1", "BMEVIEEAA00", 7, 1, "Dr. Czirkos Zoltán"),
-                new Subject(2, "Bevezetés a számításelméletbe 1", "BMEVISZAA00", 4, 1, "Dr. Szeszlér Dávid")
-            };
+                Client.BaseAddress = new Uri(ApiBaseAddress);
+            }
+
+            Client.DefaultRequestHeaders.Accept.Clear();
+            Client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            var response = Client.GetAsync("api/Subject").Result;
+            var stringData = response.Content.ReadAsStringAsync().Result;
+            var data = JsonConvert.DeserializeObject<List<Subject>>(stringData);
+
+            return data;
         }
 
         public List<Subject> SearchSubjects(string keyword)
         {
-            throw new NotImplementedException();
+            if (Client.BaseAddress == null)
+            {
+                Client.BaseAddress = new Uri(ApiBaseAddress);
+            }
+
+            Client.DefaultRequestHeaders.Accept.Clear();
+            Client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            var response = Client.GetAsync("api/Subject/" + keyword).Result;
+            var stringData = response.Content.ReadAsStringAsync().Result;
+            var data = JsonConvert.DeserializeObject<List<Subject>>(stringData);
+
+            return data;
         }
 
         public List<Course> GetCoursesOfSubject(int subjectId)
         {
-            throw new NotImplementedException();
+            if (Client.BaseAddress == null)
+            {
+                Client.BaseAddress = new Uri(ApiBaseAddress);
+            }
+
+            Client.DefaultRequestHeaders.Accept.Clear();
+            Client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            var response = Client.GetAsync("api/Course/" + subjectId).Result;
+            var stringData = response.Content.ReadAsStringAsync().Result;
+            var data = JsonConvert.DeserializeObject<List<Course>>(stringData);
+
+            return data;
         }
 
         public List<Grade> GetGradesOfSemester(int semester)
         {
-            throw new NotImplementedException();
+            if (Client.BaseAddress == null)
+            {
+                Client.BaseAddress = new Uri(ApiBaseAddress);
+            }
+
+            Client.DefaultRequestHeaders.Accept.Clear();
+            Client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            var response = Client.GetAsync("api/Grade/" + semester).Result;
+            var stringData = response.Content.ReadAsStringAsync().Result;
+            var data = JsonConvert.DeserializeObject<List<Grade>>(stringData);
+
+            return data;
         }
     }
 }
