962762ae967d2a7706002dc062f1203d9502408a
Peter Kiraly <zsonglorman@gmail.com>
2017-10-08 22:15:43 +0200

    Get grades of a semester

---
+++ Poseidon/Interfaces/Grade.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Interfaces
+{
+    public class Grade
+    {
+        public int StudentID { get; set; }
+
+        public int SubjectID { get; set; }
+
+        public int EnrollmentSemenster { get; set; }
+
+        public bool Signature { get; set; }
+
+        public bool Passed { get; set; }
+
+        public int ReceivedGrade { get; set; }
+
+        public Grade(int studentID, int subjectID, int enrollmentSemenster, bool signature, bool passed, int receivedGrade)
+        {
+            StudentID = studentID;
+            SubjectID = subjectID;
+            EnrollmentSemenster = enrollmentSemenster;
+            Signature = signature;
+            Passed = passed;
+            ReceivedGrade = receivedGrade;
+        }
+    }
+}
