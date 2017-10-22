0329d20b819a77a6437f60610d7c28136a204d36
Krisztian Lengyel <lengyel.krisztian@gmail.com>
2017-10-22 15:51:20 +0200

    Updated view

--- Poseidon/AspNetClient/Startup.cs
+++ Poseidon/AspNetClient/Startup.cs
@@ -29,6 +29,7 @@ namespace AspNetClient
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
