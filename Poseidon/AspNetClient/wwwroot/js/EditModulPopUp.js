15e8ac94791e5d2037a00aeb2f7e6ae1d726f9ed
HegyiAdam <hegyia02@gmail.com>
2017-11-04 16:38:42 +0100

    Js codes extracted

---
+++ Poseidon/AspNetClient/wwwroot/js/EditModulPopUp.js
@@ -0,0 +1,12 @@
+﻿function getPartialView(id, url) {
+    url = url.concat("/" + id);
+    $.ajax({
+        url: url,
+        type: "GET",
+        cache: false,
+        success: function (result) {
+            $("#SubjectModify").html(result)
+        }
+    });
+    $("#myModal").modal();
+}
---
+++ Poseidon/AspNetClient/wwwroot/js/Subjects_Index.js
@@ -0,0 +1,34 @@
+﻿$("#sign").click(function () {
+    $("#mark").attr("disabled", !this.checked);
+    $("#mark").val("1");
+
+});
+
+function getPartialView(id,url) {
+    url = url.concat("/" + id);
+    $.ajax({
+        url: url,
+        type: "GET",
+        cache: false,
+        success: function (result) {
+            $("#SubjectDetails").html(result)
+        }
+    });
+    $("#sign").attr("checked", false);
+    $("#mark").val('1').attr("disabled", true);
+    $("#myModal").modal();
+    //document.getElementById("myModal").setAttribute("style", "display:normal");
+}
+
+function Submit() {
+    if (($("#mark").val() < 1) || ($("#mark").val() > 5)) {
+
+        $("#markForm").addClass("has-error");
+        return;
+    }
+
+    $("#myModal").modal('hide');
+    $("#SuccessModal").fadeTo(800, 500).slideUp(500, function () {
+        $("#SuccessModal").slideUp(200);
+    });
+}
