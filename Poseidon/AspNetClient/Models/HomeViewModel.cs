8e625a0126af127980a4c59bedaf413837b50c35
Krisztian Lengyel <lengyel.krisztian@gmail.com>
2017-11-19 23:08:03 +0100

    Statistics Added

--- Poseidon/AspNetClient/Models/HomeViewModel.cs
+++ Poseidon/AspNetClient/Models/HomeViewModel.cs
@@ -9,6 +9,51 @@ namespace AspNetClient.Models
     public class HomeViewModel
     {
         public IList<Interfaces.SubjectWithGrade> SubjectWithGrade { get; private set; }
+        public int AllCredits
+        {
+            get
+            {
+                int sum = 0;
+                foreach (var item in SubjectWithGrade)
+                    sum += item.Subject.Credit;
+                return sum;
+            }
+        }
+        public int EarnedCredits
+        {
+            get
+            {
+                int sum = 0;
+                foreach (var item in SubjectWithGrade)
+                    if (item.Grade.Passed)
+                        sum += item.Subject.Credit;
+                return sum;
+            }
+        }
+        public double WeightedAverage
+        {
+            get
+            {
+                double average = 0;
+                foreach (var item in SubjectWithGrade)
+                    average += item.Grade.ReceivedGrade * item.Subject.Credit;
+                if (average == 0) return 0;
+                if (EarnedCredits == 0) return 1;
+                return Math.Round(average / EarnedCredits, 2);
+            }
+        }
+        public double ScholarshipAverage
+        {
+            get
+            {
+                double average = 0;
+                foreach (var item in SubjectWithGrade)
+                    average += item.Grade.ReceivedGrade * item.Subject.Credit;
+                if (average == 0) return 0;
+                return Math.Round(average / 30, 2);
+            }
+        }
+
         public static int Semester { get; set; }
 
         public HomeViewModel(List<SubjectWithGrade> subjectWithGrade, int semester)
